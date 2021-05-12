b1e7fad9dac949f27208b5cae83fea12b992c3b8
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-05-12 09:38:25 -0400

    Added lots of comments
    I tried to explain a lot of the thing I wrote.
    Also added a welcome screen.

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -5,19 +5,29 @@
 #define STANDARD_LEVEL_WIDTH 60
 #define STANDARD_LEVEL_HEIGHT 20
 
+/********************************************
+ *              Dungeon Crawler             *
+ * A text based dungeon crawler written in  *
+ * C++.                                     *
+ *                                          *
+ * Author: Graham Joonsar                   *
+ * ******************************************/
+
+/* These are what the Objects of in the game look like */
 // Player : $
 // HealthPack : H
 // Enemy : X
 // Portal Entrance : @
 // Portal Exit : &
 
-// Colors
+// Colors for the console 
 const unsigned short BRIGHT_BLUE = 11;
 const unsigned short BRIGHT_RED = 12;
 const unsigned short BRIGHT_GREEN = 10;
 const unsigned short BRIGHT_YELLOW = 14;
 const unsigned short WHITE = 15;
 
+/* This functions sets the color of the characters outputted to the console */
 void setConsoleColour(unsigned short colour)
 {
     static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -25,36 +35,51 @@ void setConsoleColour(unsigned short colour)
     SetConsoleTextAttribute(hOut, colour);
 }
 
+// This is what causes the main game loop
 bool running = true;
+// The round number // Used for moving the enemies every three rounds
 long int roundNum = 0;
 
+// This is the main player class
 class Player{
     public:
-        char display = '$';
+        char display = '$'; // what the player looks like on the console
         // Putting this in one statement didn't work for some reason
+        // x position
         int x = 1;
+        // y position
         int y = 1;
+        // The player's health
         int health = 5;
-
+        
+        // This function if for test if the player is at a specific position 
         bool isPlayerIn(int testingX, int testingY){
             return x == testingX && y == testingY;
         }
-
+        
+        // Checks if the player is dead
         void checkSelf(){
             if (health <= 0){
                 running = false;
             }
         }
 };
+// The Player declaration 
 Player player;
 
 class Level{
     public:
+        // Name of the level
         const char *name;
+        // The grid of chars that will be displayed
         char selfDisplay[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH];
-        int portalDestination; // The index of the level the portal goes to
+        // The index of the level the portal goes to
+        int portalDestination;
+        // The index that the level is // Used for updating the level when the player leaves
         int selfIndex;
+        // The position of the entrance to the level
         int entranceX, entranceY;
+    // The constructor
     Level(char _selfDisplay[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH], const char * _name, int _portalDestination, int _newX, int _newY, int _selfIndex){
         // For some reason I can't assign this directly
         for (int y = 0; y < STANDARD_LEVEL_HEIGHT; y++){
@@ -70,8 +95,10 @@ class Level{
     }
 };
 
+// the top and bottom of every level 
 char topAndBottom[STANDARD_LEVEL_WIDTH] = "###########################################################";
 
+// The maze level display
 char Level1Disp[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH] = {
     "###########################################################",
     "#&          ###         ###         ###                   #",
@@ -96,6 +123,7 @@ char Level1Disp[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH] = {
 };
 
 
+// The default level display
 char LevelDefaultDisp[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH] = {
     "###########################################################",
     "#&                                                        #",
@@ -123,13 +151,17 @@ char LevelDefaultDisp[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH] = {
 // The level that will be displayed on screen
 Level * dispLevel;
 
+// The maze level
 Level MazeLevel(Level1Disp, "Maze", 0, 1, 1, 1);
+// The default empty level
 Level EmptyLevel(LevelDefaultDisp, "Empty Level", 1, 1, 1, 0);
 
+// The list of all levels // used for changing between levels
 Level allLevels[] = {EmptyLevel, MazeLevel};
 
 void updateEnemies(){
     for (int y = 1; y < STANDARD_LEVEL_HEIGHT - 1; y++){ // For every char in the level
+        // These are used for stopping the loop from moving the enemies more than once per player movement
         bool wentRight = false;
         bool wentDown = false;
         for (int x = 0; x < STANDARD_LEVEL_WIDTH - 1; x++){
@@ -147,6 +179,7 @@ void updateEnemies(){
                 }
                 dispLevel->selfDisplay[y][x] = ' ';
                 
+                // If the player isn't where the enemy wants to move
                 if (!player.isPlayerIn(targetX, targetY)){
                     if (wentRight){
                         targetX -= 1;
@@ -173,7 +206,9 @@ void updateEnemies(){
     }
 }
 
+// Getting the player's input from the keyboard
 void getInput(){
+    // Gets what the player pressed
     char key = _getch();
     int targetx = player.x;
     int targety = player.y;
@@ -192,10 +227,11 @@ void getInput(){
     if (key == 'q'){
         running = false;
     }
+    // If where the player wants to move is empty space or a portal exit
     if (dispLevel->selfDisplay[targety][targetx] == ' ' || dispLevel->selfDisplay[targety][targetx] == '&'){
         player.x = targetx;
         player.y = targety;
-    } else if (dispLevel->selfDisplay[targety][targetx] == 'H'){
+    } else if (dispLevel->selfDisplay[targety][targetx] == 'H'){ // If where the player wants to move is a health pack
         dispLevel->selfDisplay[targety][targetx] = ' ';
         player.health++;
         player.x = targetx;
@@ -210,12 +246,14 @@ void getInput(){
 }
 
 void draw(){
+    // Clearing the console
     system("CLS");
     setConsoleColour(WHITE);
+    // Outputting the top of the level
     std::cout << topAndBottom << std::endl;
-    for (int y = 1; y < STANDARD_LEVEL_HEIGHT - 1; y++){
+    for (int y = 1; y < STANDARD_LEVEL_HEIGHT - 1; y++){ // for every line
         putchar('#');
-        for (int x = 1; x < STANDARD_LEVEL_WIDTH - 2; x++){
+        for (int x = 1; x < STANDARD_LEVEL_WIDTH - 2; x++){ // for every char on the line
             if (!player.isPlayerIn(x, y)){
                 char currentChar = dispLevel->selfDisplay[y][x];
                 if (currentChar == 'H'){
@@ -240,7 +278,7 @@ void draw(){
         putchar('#'); // the sides will always be #, so this makes it more efficient
         putchar('\n'); // the entire line has been looped through so move to the next
     }
-    std::cout << topAndBottom;
+    std::cout << topAndBottom; // printing out the bottom
     setConsoleColour(BRIGHT_GREEN);
     std::cout << "Health: " << player.health;
     setConsoleColour(WHITE);
@@ -255,7 +293,11 @@ int main(){
     MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 510, 395, TRUE);
 
     setConsoleColour(WHITE);
+    // Setting the starter level to the maze level
     dispLevel = &MazeLevel;
+    // Drawing the Welcome screen
+    std::cout << "Welcome to DungeonCrawler\n";
+    std::cout << "Press any key to start\n";
     while(running){
         if (_kbhit()){ // If a key is pressed
             roundNum++;
