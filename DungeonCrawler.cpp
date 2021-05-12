07df4cc83ddc79c758be5818b4ae41a680f02d0c
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-05-11 20:47:29 -0400

    Added enemy movement
    The enemy will now move towards the player. It does not work when the player is below the enemy, though.

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -26,6 +26,7 @@ void setConsoleColour(unsigned short colour)
 }
 
 bool running = true;
+long int roundNum = 0;
 
 class Player{
     public:
@@ -37,6 +38,12 @@ class Player{
 
         bool isPlayerIn(int testingX, int testingY){
             return x == testingX && y == testingY;
+        }
+
+        void checkSelf(){
+            if (health <= 0){
+                running = false;
+            }
         }
 };
 Player player;
@@ -69,7 +76,7 @@ char Level1Disp[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH] = {
     "###########################################################",
     "#&          ###         ###         ###                   #",
     "#           ###         ###         ###                   #",
-    "#           ###         ###         ###                   #",
+    "#@          ###         ###         ###                   #",
     "#           ###         ###         ###                   #",
     "#           ###         ###         ###                   #",
     "#     ###   ###   ###   ###   ###   ###   ###             #",
@@ -98,15 +105,15 @@ char LevelDefaultDisp[STANDARD_LEVEL_HEIGHT][STANDARD_LEVEL_WIDTH] = {
     "#                                                         #",
     "#                                                         #",
     "#                                                         #",
-    "#                           X                             #",
-    "#            @                                            #",
-    "#                           X                             #",
+    "#                           X X                           #",
+    "#            @              X X                           #",
+    "#                           X X                           #",
     "#                                                         #",
     "#                                                         #",
     "#                                                         #",
     "#                                                         #",
     "#                                                         #",
-    "#                                                         #",
+    "#                                X                        #",
     "#                                                       HH#",
     "#                                                       HH#",
     "###########################################################"
@@ -120,6 +127,51 @@ Level MazeLevel(Level1Disp, "Maze", 0, 1, 1, 1);
 Level EmptyLevel(LevelDefaultDisp, "Empty Level", 1, 1, 1, 0);
 
 Level allLevels[] = {EmptyLevel, MazeLevel};
+
+void updateEnemies(){
+    for (int y = 1; y < STANDARD_LEVEL_HEIGHT - 1; y++){ // For every char in the level
+        bool wentRight = false;
+        bool wentDown = false;
+        for (int x = 0; x < STANDARD_LEVEL_WIDTH - 1; x++){
+            if (dispLevel->selfDisplay[y][x] == 'X' && roundNum % 3 == 0){ // If enemy // Updates every 3 rounds
+                int targetX = x;
+                int targetY = y;
+                if (x > player.x && dispLevel->selfDisplay[y][x - 1] == ' '){ // Move left
+                    targetX -= 1;
+                } else if (x < player.x && dispLevel->selfDisplay[y][x + 1] == ' '){ // move right
+                    targetX += 1;
+                } else if (y > player.y && dispLevel->selfDisplay[y - 1][x] == ' '){ // Move up
+                    targetY -= 1;
+                } else if (y < player.y && dispLevel->selfDisplay[y + 1][x] == ' '){ // move down
+                    targetY += 1;
+                }
+                dispLevel->selfDisplay[y][x] = ' ';
+                
+                if (!player.isPlayerIn(targetX, targetY)){
+                    if (wentRight){
+                        targetX -= 1;
+                    }
+                    if (wentDown){
+                        targetY -= 1;
+                    }
+                    dispLevel->selfDisplay[targetY][targetX] = 'X';
+                } else {
+                    player.health -= 1;
+                }
+
+                wentRight = false;
+                wentDown = false;
+
+                if (targetX > x){
+                    wentRight = true;
+                }
+                if (targetY > y){
+                    wentDown = true;
+                }
+            }
+        }
+    }
+}
 
 void getInput(){
     char key = _getch();
@@ -148,11 +200,6 @@ void getInput(){
         player.health++;
         player.x = targetx;
         player.y = targety;
-    } else if (dispLevel->selfDisplay[targety][targetx] == 'X'){
-        dispLevel->selfDisplay[targety][targetx] = ' ';
-        player.health--;
-        player.x = targetx;
-        player.y = targety;
     } else if (dispLevel->selfDisplay[targety][targetx] == '@'){
         allLevels[dispLevel->selfIndex] = *dispLevel; // This saves what the player did in the level
         *dispLevel = allLevels[dispLevel->portalDestination]; // this updates the level
@@ -170,18 +217,19 @@ void draw(){
         putchar('#');
         for (int x = 1; x < STANDARD_LEVEL_WIDTH - 2; x++){
             if (!player.isPlayerIn(x, y)){
-                if (dispLevel->selfDisplay[y][x] == 'H'){
+                char currentChar = dispLevel->selfDisplay[y][x];
+                if (currentChar == 'H'){
                     setConsoleColour(BRIGHT_GREEN);
                     putchar('H');
-                } else if (dispLevel->selfDisplay[y][x] == 'X'){
+                } else if (currentChar == 'X'){
                     setConsoleColour(BRIGHT_RED);
                     putchar('X');
-                } else if (dispLevel->selfDisplay[y][x] == '@' || dispLevel->selfDisplay[y][x] == '&'){
+                } else if (currentChar == '@' || currentChar == '&'){
                     setConsoleColour(BRIGHT_YELLOW);
-                    putchar(dispLevel->selfDisplay[y][x]);
+                    putchar(currentChar);
                 } else {
                     setConsoleColour(WHITE);
-                    putchar(dispLevel->selfDisplay[y][x]);
+                    putchar(currentChar);
                 }
             } else {
                 setConsoleColour(BRIGHT_BLUE);
@@ -209,9 +257,17 @@ int main(){
     setConsoleColour(WHITE);
     dispLevel = &MazeLevel;
     while(running){
-        // Drawing the level with the player inside
-        draw();
-        // Getting input
-        getInput();
+        if (_kbhit()){ // If a key is pressed
+            roundNum++;
+            // Getting input
+            getInput();
+            // Moving the enemies
+            updateEnemies();
+            // Checking the player health
+            player.checkSelf();
+            // Drawing the level with the player inside
+            draw();
+        }
+
     }
 }
