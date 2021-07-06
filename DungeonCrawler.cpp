8f619704634b3672bc41ff9ea97b60a90d4281fd
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-07-06 19:02:41 -0400

    Added player movement and fixed the screen flicker in the previous version

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -1,5 +1,111 @@
-# include <iostream>
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+const int LEVEL_WIDTH = 60;
+const int LEVEL_HEIGHT = 12;
+
+char keyPressed = 'l';
+bool keyBeenPressed = false;
+bool running = true;
+
+// The level struct, contains information about the current level
+struct Level{
+    char DispLevel[LEVEL_HEIGHT][LEVEL_WIDTH];
+
+    Level(char _dp[LEVEL_HEIGHT][LEVEL_WIDTH]){
+        for (int y = 0; y < LEVEL_HEIGHT; y++){
+            for(int x = 0; x < LEVEL_WIDTH; x++){
+                DispLevel[y][x] = _dp[y][x];
+            }
+        }
+    }
+};
+
+// Player class
+class Player{
+    public:
+        char disp = '$'; // What the player looks like on the console
+        int x = 1, y = 1;
+
+        void ProccessInput(char in, Level * l){
+            int targX = x, targY = y;
+            switch(in){
+                case 'w':
+                    targY--;
+                    break;
+                case 's':
+                    targY++;
+                    break;
+                case 'a':
+                    targX--;
+                    break;
+                case 'd':
+                    targX++;
+                    break;
+                case 'q':
+                    running = false;
+                    break;
+            }
+            if (l->DispLevel[targY][targX] == ' '){ // Clear space
+                x = targX;
+                y = targY;
+            }
+        }
+};
+
+char TestDisp[LEVEL_HEIGHT][LEVEL_WIDTH] = {
+    "###########################################################",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "#                                                         #",
+    "###########################################################"
+};
+
+Level Test(TestDisp);
+
+void DrawToConsole(Level * leveltoDraw, Player * p){
+    for (int y = 0; y < LEVEL_HEIGHT; y++){
+        for (int x = 0; x < LEVEL_WIDTH; x++){
+            if (x == p->x && y == p->y){
+                std::cout << p->disp;
+            } else {
+                std::cout << leveltoDraw->DispLevel[y][x];
+            }
+        }
+        std::cout << std::endl; // Newline
+    }
+}
+
+void ClearConsole(){
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout.flush();
+    COORD coord = {0, 0};
+    SetConsoleCursorPosition(hOut, coord);
+}
+
+Level * currentLevel;
+
+Player player;
 
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    system("cls");
+    currentLevel = &Test;
+    while (running){
+        if (_kbhit()){ // Key pressed
+            keyPressed = _getch();
+            player.ProccessInput(keyPressed, currentLevel);
+            DrawToConsole(currentLevel, &player);
+            ClearConsole();
+            //std::cout << keyBeenPressed << std::endl;
+        }
+    }
+    system("cls");
 }
--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 cls
 echo Starting Build
 if %1 == debug g++ -Wall DungeonCrawler.cpp -o DungeonCrawler.exe
-if %1 == release g++ -static-libstdc++ DungeonCrawler.cpp -o DungeonCrawler.exe
+if %1 == release g++ -O3 -static-libstdc++ DungeonCrawler.cpp -o DungeonCrawler.exe
 echo Build Complete
