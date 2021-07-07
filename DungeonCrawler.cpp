89ee3dc9a12a6c9d20ac555aec3b2f3a1bdd1226
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-07-06 21:59:05 -0400

    Added the health at the bottom and Health packs

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -5,28 +5,55 @@
 const int LEVEL_WIDTH = 60;
 const int LEVEL_HEIGHT = 12;
 
-char keyPressed = 'l';
+// Console colors
+const unsigned short BRIGHT_BLUE = 11;
+const unsigned short BRIGHT_RED = 12;
+const unsigned short BRIGHT_GREEN = 10;
+const unsigned short BRIGHT_YELLOW = 14;
+const unsigned short BRIGHT_PINK = 13;
+const unsigned short WHITE = 15;
+
+char keyPressed;
 bool keyBeenPressed = false;
 bool running = true;
+
+// Console Functions
+void ClearConsole(){
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout.flush();
+    COORD coord = {0, 0};
+    SetConsoleCursorPosition(hOut, coord);
+}
+
+void setConsoleColour(unsigned short color){
+    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout.flush();
+    SetConsoleTextAttribute(hOut, color);
+}
 
 // The level struct, contains information about the current level
 struct Level{
     char DispLevel[LEVEL_HEIGHT][LEVEL_WIDTH];
+    const char * lname;
 
-    Level(char _dp[LEVEL_HEIGHT][LEVEL_WIDTH]){
+    // Constructor
+    Level(char _dp[LEVEL_HEIGHT][LEVEL_WIDTH], const char * _lname){
         for (int y = 0; y < LEVEL_HEIGHT; y++){
             for(int x = 0; x < LEVEL_WIDTH; x++){
                 DispLevel[y][x] = _dp[y][x];
             }
         }
+        lname = _lname;
     }
 };
 
 // Player class
 class Player{
+    private:
     public:
         char disp = '$'; // What the player looks like on the console
         int x = 1, y = 1;
+        unsigned int health = 9;
 
         void ProccessInput(char in, Level * l){
             int targX = x, targY = y;
@@ -46,8 +73,16 @@ class Player{
                 case 'q':
                     running = false;
                     break;
+                case 'x':
+                    health--;
+                    break;
             }
             if (l->DispLevel[targY][targX] == ' '){ // Clear space
+                x = targX;
+                y = targY;
+            } else if (l->DispLevel[targY][targX] == 'H'){ // Health pack
+                l->DispLevel[targY][targX] = ' ';
+                health++;
                 x = targX;
                 y = targY;
             }
@@ -61,7 +96,7 @@ char TestDisp[LEVEL_HEIGHT][LEVEL_WIDTH] = {
     "#                                                         #",
     "#                                                         #",
     "#                                                         #",
-    "#                                                         #",
+    "#                                                       H #",
     "#                                                         #",
     "#                                                         #",
     "#                                                         #",
@@ -69,43 +104,67 @@ char TestDisp[LEVEL_HEIGHT][LEVEL_WIDTH] = {
     "###########################################################"
 };
 
-Level Test(TestDisp);
+Level Test(TestDisp, "test");
 
 void DrawToConsole(Level * leveltoDraw, Player * p){
+    // Looping through level
     for (int y = 0; y < LEVEL_HEIGHT; y++){
         for (int x = 0; x < LEVEL_WIDTH; x++){
-            if (x == p->x && y == p->y){
+            char currentChar = leveltoDraw->DispLevel[y][x];
+            if (x == p->x && y == p->y){ // If its the player
+                setConsoleColour(BRIGHT_BLUE);
                 std::cout << p->disp;
+            } else if (currentChar == 'H'){
+                setConsoleColour(BRIGHT_GREEN);
+                std::cout << 'H';
             } else {
+                setConsoleColour(WHITE);
                 std::cout << leveltoDraw->DispLevel[y][x];
             }
         }
         std::cout << std::endl; // Newline
     }
+    std::cout << "HEALTH: ";
+    setConsoleColour(BRIGHT_GREEN);
+    std::cout << p->health;
+    setConsoleColour(WHITE);
+    std::cout << "  |  Level: ";
+    std::cout << leveltoDraw->lname;
+    std::cout << "                       " << std::endl; // Clearing the chars
+
+    setConsoleColour(WHITE);
 }
 
-void ClearConsole(){
-    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    std::cout.flush();
-    COORD coord = {0, 0};
-    SetConsoleCursorPosition(hOut, coord);
-}
 
+// The level thats being displayed
 Level * currentLevel;
-
 Player player;
 
 int main(){
+    currentLevel = &Test; // Setting the level
+    
+    // Hiding the cursor
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO CursorInfo;
+    GetConsoleCursorInfo(handle, &CursorInfo); // Get console cursor information
+    CursorInfo.bVisible = false; // Hide the console cursor
+    SetConsoleCursorInfo(handle, &CursorInfo); // Set the console cursor state
+
+    system("title Dungeon Crawler"); // Setting the title of the console window
+    system("cls"); // Clearing the screen
+
+    std::cout << "Welcome to Dungeon Crawler!" << std::endl;
+    system("pause");
     system("cls");
-    currentLevel = &Test;
-    while (running){
+    player.x = player.y = 1;
+
+    while (running){ // Main game loop
         if (_kbhit()){ // Key pressed
-            keyPressed = _getch();
-            player.ProccessInput(keyPressed, currentLevel);
-            DrawToConsole(currentLevel, &player);
-            ClearConsole();
-            //std::cout << keyBeenPressed << std::endl;
+            player.ProccessInput(_getch(), currentLevel); // Proccess the input
+            DrawToConsole(currentLevel, &player); // Drawing the level to console
+            ClearConsole(); // Clearing the console without flicker
         }
     }
-    system("cls");
+    system("cls"); // Clearing screen again
+    std::cout << "Thanks for playing!" << std::endl;
 }
