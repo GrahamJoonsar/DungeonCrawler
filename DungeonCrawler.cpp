15f6a4c039fd0729a0b33d866407263744e31b1a
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-07-26 21:56:22 -0400

    Added enemies and made it so the player and enemy classes can be defined before the level struct

--- DungeonCrawler.cpp
+++ DungeonCrawler.cpp
@@ -5,6 +5,8 @@
 const int LEVEL_WIDTH = 60;
 const int LEVEL_HEIGHT = 12;
 
+unsigned int turnNumber = 0;
+
 // Console colors
 const unsigned short BRIGHT_BLUE = 11;
 const unsigned short BRIGHT_RED = 12;
@@ -31,31 +33,15 @@ void setConsoleColour(unsigned short color){
     SetConsoleTextAttribute(hOut, color);
 }
 
-// The level struct, contains information about the current level
-struct Level{
-    char DispLevel[LEVEL_HEIGHT][LEVEL_WIDTH];
-    const char * lname;
-
-    // Constructor
-    Level(char _dp[LEVEL_HEIGHT][LEVEL_WIDTH], const char * _lname){
-        for (int y = 0; y < LEVEL_HEIGHT; y++){
-            for(int x = 0; x < LEVEL_WIDTH; x++){
-                DispLevel[y][x] = _dp[y][x];
-            }
-        }
-        lname = _lname;
-    }
-};
-
 // Player class
 class Player{
     private:
     public:
         char disp = '$'; // What the player looks like on the console
         int x = 1, y = 1;
-        unsigned int health = 9;
+        unsigned short health = 9;
 
-        void ProccessInput(char in, Level * l){
+        void ProccessInput(char in, char DispLevel[][60]){
             int targX = x, targY = y;
             switch(in){
                 case 'w':
@@ -77,15 +63,78 @@ class Player{
                     health--;
                     break;
             }
-            if (l->DispLevel[targY][targX] == ' '){ // Clear space
+            if (DispLevel[targY][targX] == ' '){ // Clear space
                 x = targX;
                 y = targY;
-            } else if (l->DispLevel[targY][targX] == 'H'){ // Health pack
-                l->DispLevel[targY][targX] = ' ';
+            } else if (DispLevel[targY][targX] == 'H'){ // Health pack
+                DispLevel[targY][targX] = ' ';
                 health++;
                 x = targX;
                 y = targY;
             }
+        }
+};
+
+class Enemy{
+    public:
+        int x, y;
+        bool dead = false;
+
+        Enemy(int _x, int _y){
+            x = _x;
+            y = _y;
+        }
+
+        void move(char DispLevel[][60], Player * p){
+            int targy = y, targx = x;
+            if (p->y < y){ // Moving up
+                targy--;
+            } else if (p->y > y){
+                targy++;
+            } else if (p->x < x){
+                targx--;
+            } else {
+                targx++;
+            }
+            if (DispLevel[targy][targx] == ' '){ // Clear space
+                x = targx;
+                y = targy;
+            } else if (p->x == x && p->y == y){ // Player
+                p->health--;
+                dead = true;
+            }
+        }
+};
+
+// The level struct, contains information about the current level
+struct Level{
+    public:
+        char DispLevel[LEVEL_HEIGHT][LEVEL_WIDTH]; // The char grid that represents the level
+        const char * lname; // The name of the level
+        Enemy * enemies; // the Enemies in the level
+        int ENum; // Number of enemies
+
+        // Constructor
+        Level(char _dp[LEVEL_HEIGHT][LEVEL_WIDTH], const char * _lname, Enemy * _es, int _en){
+            for (int y = 0; y < LEVEL_HEIGHT; y++){
+                for(int x = 0; x < LEVEL_WIDTH; x++){
+                    DispLevel[y][x] = _dp[y][x];
+                }
+            }
+            lname = _lname;
+            enemies = _es;
+            ENum = _en;
+        }
+
+        bool EnemyAt(int x, int y){ // Checking if an enemy is at a position
+            for (int i = 0; i < ENum; i++){
+                if (enemies[i].x != x){
+                    continue;
+                } else {
+                    return enemies[i].y == y && !enemies[i].dead;
+                }
+            }
+            return false;
         }
 };
 
@@ -104,7 +153,9 @@ char TestDisp[LEVEL_HEIGHT][LEVEL_WIDTH] = {
     "###########################################################"
 };
 
-Level Test(TestDisp, "test");
+Enemy TestEnemies[] = {{30, 6}};
+
+Level Test(TestDisp, "test", TestEnemies, 1);
 
 void DrawToConsole(Level * leveltoDraw, Player * p){
     // Looping through level
@@ -117,6 +168,9 @@ void DrawToConsole(Level * leveltoDraw, Player * p){
             } else if (currentChar == 'H'){
                 setConsoleColour(BRIGHT_GREEN);
                 std::cout << 'H';
+            } else if (leveltoDraw->EnemyAt(x, y)){
+                setConsoleColour(BRIGHT_RED);
+                std::cout << 'X';
             } else {
                 setConsoleColour(WHITE);
                 std::cout << leveltoDraw->DispLevel[y][x];
@@ -156,11 +210,14 @@ int main(){
     std::cout << "Welcome to Dungeon Crawler!" << std::endl;
     system("pause");
     system("cls");
-    player.x = player.y = 1;
+    player.x = player.y = 1; // top left
 
     while (running){ // Main game loop
         if (_kbhit()){ // Key pressed
-            player.ProccessInput(_getch(), currentLevel); // Proccess the input
+            player.ProccessInput(_getch(), currentLevel->DispLevel); // Proccess the input
+            for (int i = 0; i < currentLevel->ENum; i++){
+                currentLevel->enemies[i].move(currentLevel->DispLevel, &player);
+            }
             DrawToConsole(currentLevel, &player); // Drawing the level to console
             ClearConsole(); // Clearing the console without flicker
         }
--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 cls
 echo Starting Build
-if %1 == debug g++ -Wall DungeonCrawler.cpp -o DungeonCrawler.exe
-if %1 == release g++ -O3 -static-libstdc++ DungeonCrawler.cpp -o DungeonCrawler.exe
+if %%1 == debug g++ -Wall DungeonCrawler.cpp -o DungeonCrawler.exe
+if %%1 == release g++ -O3 -static-libstdc++ DungeonCrawler.cpp -o DungeonCrawler.exe
 echo Build Complete
